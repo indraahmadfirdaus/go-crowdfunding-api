222a41b0e4ef08fba284d083aa7c6bdbaf9f1328
Indra <indraahmadfirdaus@gmail.com>
2023-11-24 17:05:28 +0700

    jwt auth

--- auth/service.go
+++ auth/service.go
@@ -1,9 +1,14 @@
 package auth
 
-import "github.com/golang-jwt/jwt/v5"
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type Service interface {
 	GenerateToken(userId int) (string, error)
+	ValidateToken(token *jwt.Token) (*jwt.Token, error)
 }
 
 type jwtService struct{}
@@ -27,4 +32,20 @@ func (s *jwtService) GenerateToken(userID int) (string, error) {
 	}
 
 	return signedToken, err
+}
+
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(SECRET), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
 }
